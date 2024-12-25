8a311d7079029a9a171778e7a851157fc0fac43a
EmilMustafaev <emilmustafaev891@gmail.com>
2024-12-25 11:39:58 +0300

    1. Фильтры для объявлений машин [#4745]

---
+++ src/main/java/ru/job4j/cars/repository/post/PostRepository.java
@@ -0,0 +1,39 @@
+package ru.job4j.cars.repository.post;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Repository;
+import ru.job4j.cars.model.Post;
+import ru.job4j.cars.repository.CrudRepository;
+import java.time.LocalDateTime;
+
+import java.util.List;
+import java.util.Map;
+
+@AllArgsConstructor
+@Repository
+public class PostRepository {
+
+    private final CrudRepository crudRepository;
+
+    public List<Post> findPostsLastDay() {
+        String query = """
+            FROM Post p WHERE p.created >= :yesterday
+        """;
+        return crudRepository.query(query, Post.class, Map.of("yesterday", LocalDateTime.now().minusDays(1)));
+    }
+
+    public List<Post> findPostsWithPhotos() {
+        String query = """
+            FROM Post p WHERE size(p.priceHistory) > 0
+        """;
+        return crudRepository.query(query, Post.class);
+    }
+
+    public List<Post> findPostsByCarBrand(String brandName) {
+        String query = """
+            FROM Post p WHERE p.car.name = :brandName
+        """;
+        return crudRepository.query(query, Post.class, Map.of("brandName", brandName));
+    }
+
+}
